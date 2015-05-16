0658a65c03792648599481f57d8610e6492ce10c
Joon-Sub Chung <Joon-Sub.Chung@zazzle.com>
2015-05-16 13:29:32 -0700

    initial commit jsub

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+
+document.getElementById("mobileMenu").onclick = function(){
+	document.getElementById('offCanvasMenu').classList.toggle('active');
+	document.getElementById('main').classList.toggle('active');
+}
