a8eb3396e82489e8b6440488094f0d9e9eb4012e
Joon-Sub Chung <joonsubtalk@gmail.com>
2016-04-10 18:56:01 -0700

    gulp flow

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,46 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./assets/css/'));
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+gulp.task('sass', function () {
+  return gulp
+    // Find all `.scss` files from the `stylesheets/` folder
+    .src('./sass/**/*.scss')
+    // Run Sass on those files
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    //.pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    // Write the resulting CSS in the output folder
+    .pipe(gulp.dest('./assets/css/'))
+    .pipe(livereload());
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+
+// gulp.task('default',function() {
+//     gulp.watch('sass/**/*.scss',['styles']);
+// });
+
+gulp.task('watch', function() {
+  livereload.listen();
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch('./sass/**/*.scss', ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
+
+gulp.task('default', ['sass', 'watch' /*, possible other tasks... */]);
--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("mobileMenu").onclick = function(){
-	document.getElementById('offCanvasMenu').classList.toggle('active');
-	document.getElementById('main').classList.toggle('active');
-}
