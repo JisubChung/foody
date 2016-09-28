22f804e9b20113a2acf40c034de07013ea144c43
joonsub <joonsubtalk@gmail.com>
2016-09-28 14:58:33 -0700

    gulp changes

---
+++ test.js
@@ -0,0 +1,115 @@
+www.recurse.com
+
+functional programming (JS)
+
+What is functional programming:
+1. programming paradigm
+
+Functions are king!
+2. Style of coding: 
+3. A mindset
+
+Why Functional Javascript?
+OO JS get's tricky with this and prototype
+
+
+Safer, easier to debug, and maintain
+
+---
+    
+There's a community
+
+Do everything with functions!
+
+    input -> output
+
+Not steps, but in terms of functions
+
+
+//imperative
+var name = "joon";
+var gree = "hi, i'm ";
+
+console.log(name + gree)
+
+//functional
+function greet(name) {
+    console.log("hi, i'm " + name)
+}
+
+===================
+Avoid side effects: use pure functions!
+
+pure:
+only returns
+
+Use Higher order functions: functions that can be I/O
+
+#Don't Iterate!
+use map, reduce, filter
+** know this!
+    
+Avoid mutability: use immutable 
+
+Persistent data structures for efficient immutability
+
+Mori, Immutable.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
