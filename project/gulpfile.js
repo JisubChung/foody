d76eea9874469e91480f13044b4dd353d42fb02b
joonsub <joonsubtalk@gmail.com>
2016-09-29 15:25:53 -0700

    Added Watch for HTML,JS,scss + Added boilerplate HTML5

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp'), // look into node_modules for a folder named "gulp"
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('sass', function(){
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass()) //using gulp-sass
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('watch', function(){
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']);
-  //add other watchers
+  // Reload whenever the HTML or JS changes
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 });
