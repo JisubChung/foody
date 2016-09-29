7201dcb0c799a090006bb87842ece12f67309497
joonsub <joonsubtalk@gmail.com>
2016-09-29 16:11:40 -0700

    Added 'gulp build' get's the latest distrib Added 'gulp watch' Auto Prefix + Watch

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,10 +1,21 @@
-var gulp = require('gulp'), // look into node_modules for a folder named "gulp"
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+var gulp = require('gulp'),         // look into node_modules for a folder named "gulp"
+    sass = require('gulp-sass'),    // sass, so much faster than gems
+    browserSync = require('browser-sync').create(), //now we talking!
+    useref = require('gulp-useref'),  // combine js in right order
+    uglify = require('gulp-uglify'),  // minify
+    gulpIf = require('gulp-if'),      // minify only what we want!
+    cssnano = require('gulp-cssnano'),// minify css
+    del = require('del'),             // deletes; like for outdated dist
+    runSequence = require('run-sequence'),
+    autoPrefixer = require('gulp-autoprefixer'); // makes sure order is kept
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
     .pipe(sass())
+    .pipe(autoPrefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
       stream: true
@@ -19,9 +30,40 @@ gulp.task('browserSync', function() {
   });
 });
 
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'fonts'],
+    callback
+  )
+})
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+})
+
 gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']);
   // Reload whenever the HTML or JS changes
-  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
 });
